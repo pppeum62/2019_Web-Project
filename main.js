f8cf812251710b9c968c5a3e558a02f0d789916d
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2019-12-20 20:58:47 +0900

    change ost btn
    ost 버튼을 누르면 이벤트 발생

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var upbtn = document.getElementById("upBtn");           // steel-cut up 버튼
 var downbtn = document.getElementById("downBtn");       // steel-cut down 버튼
+var playbtn = document.getElementById("play");          // ost play 버튼
+var stopbtn = document.getElementById("stop");          // ost stop 버튼
 
 var files = [ './image/steelCut(1).jpg', './image/steelCut(2).jpg', './image/steelCut(3).jpg' ];
 var arr = new Array();
@@ -61,4 +63,14 @@ downbtn.addEventListener('click', function() {
     downimg.src = arr[downindex%3].src;
 
     mainimg.style.boxShadow = "10px 10px 10px rgba(0, 0, 0, 0.35)";
+});
+
+playbtn.addEventListener('click', function() {      // play 버튼을 누르면 stop 버튼으로 바뀌는 이벤트 리스너
+    playbtn.style.display = 'none';
+    stopbtn.style.display = 'inline';
+});
+
+stopbtn.addEventListener('click', function() {      // stop 버튼을 누르면 play 버튼으로 바뀌는 이벤트 리스너
+    stopbtn.style.display = 'none';
+    playbtn.style.display = 'inline';
 });
